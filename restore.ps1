ff4b99b8cb4c53aaccafbdcb019320748034107f
Yannis Guedel <yannis.guedel@unic.com>
2014-11-11 10:41:02 +0100

    added paket to bootstrap

---
+++ restore.ps1
@@ -0,0 +1,14 @@
+$Invocation = (Get-Variable MyInvocation -Scope 0).Value
+$PSScriptRoot = Split-Path $Invocation.MyCommand.Path
+
+$paketFolder = "$PSScriptRoot\.paket"
+$paketBoot = "$paketFolder\paket.bootstrapper.exe"
+
+& $paketBoot
+& "$paketFolder\paket.exe" restore
+
+if($LASTEXITCODE -ne 0) {
+    if($env:TEAMCITY_VERSION) {
+        exit 1
+    }
+}
