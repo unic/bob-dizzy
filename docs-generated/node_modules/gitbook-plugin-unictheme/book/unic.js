c5a8fa43078ffab831df0a4a34a599ee9eab85b9
Yannis Guedel <yannis.guedel@unic.com>
2015-01-09 17:31:16 +0100

    updated keith and adapated generateDocs

--- docs-generated/node_modules/gitbook-plugin-unictheme/book/unic.js
+++
@@ -1,9 +0,0 @@
-require(["gitbook", "jQuery"], function(gitbook, $) {
-    gitbook.events.bind("start", function(e, config) {
-        $(".book-header .pull-left").last().before($("<div class='unic-logo'><img src='http://www.unic.com/img/unic-logo.png' /></div>"));
-
-        $(".font-family-list").hide()
-    });
-
-
-});
