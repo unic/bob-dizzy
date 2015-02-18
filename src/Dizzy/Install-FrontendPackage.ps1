a2af4d513b25e32fb9002498120319ec6e26f0ad
Yannis Guedel <yannis.guedel@unic.com>
2015-02-18 18:58:55 +0100

    Dizyy was not updating exsting files
    So files which already exists will be deleted befor the package gets
    extracted.

--- src/Dizzy/Install-FrontendPackage.ps1
+++ src/Dizzy/Install-FrontendPackage.ps1
@@ -27,6 +27,7 @@ function Install-FrontendPackage
     {
         Write-Verbose "Copy  files to $Location"
         $fileSystem = New-Object NuGet.PhysicalFileSystem $Location
+        $package.GetFiles().Path | % {"$Location\$_"} | ? {Test-Path $_} | % {rm $_}
         $fileSystem.AddFiles($Package.GetFiles(), $Location)
     }
 }
